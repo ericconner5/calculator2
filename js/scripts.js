46156ca0ad4312a4543bb50800bc768f0bdbc2c3
Daniel Mace and Josh Gearheart <josh@>
2018-08-08 12:04:20 -0700

    create interface for add function

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,36 @@
-// // Da Business Functions
-// var add = function(number1, number2) {
-//   return number1 + number2;
-// };
-//
-// var subtract = function(number1, number2) {
-//   return number1 - number2;
-// };
-//
-// var times = function(number1,number2) {
-// return number1*number2;
-// };
-//
-// var divide = function(number1, number2) {
-// return number1 / number2;
-// }
+// Da Business Functions
+var add = function(number1, number2) {
+  return number1 + number2;
+};
 
-//Da User Logic
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
 
-// var number1 = parseInt(prompt("Enter a number:"));
-// var number2 = parseInt(prompt("Enter another number:"));
-//
-// var result = divide(number1, number2);
-//
-// alert(result);
+var times = function(number1,number2) {
+return number1*number2;
+};
+
+var divide = function(number1, number2) {
+return number1 / number2;
+};
+
+// Da User Logic
+
+$(function () {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output p").text(result);
+  });
+});
+
+
+
+
+
 
 // var weight = parseInt(prompt("how fat are you? in pounds"));
 // var height = parseInt(prompt("how many inches tall are you?"));
@@ -44,10 +52,10 @@
 // };
 // alert(convert(fahrenCels));
 
-var celsFahren = parseInt(prompt("Enter a Celsius temp yo dog: "));
-
-var convert = function(celsFahren) {
-  return ((celsFahren*1.8)+32);
-};
-
-alert(convert(celsFahren));
+// var celsFahren = parseInt(prompt("Enter a Celsius temp yo dog: "));
+//
+// var convert = function(celsFahren) {
+//   return ((celsFahren*1.8)+32);
+// };
+//
+// alert(convert(celsFahren));
